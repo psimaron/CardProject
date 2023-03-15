9a8fab84c6b78207ea25af3fadaf61fd541eb49c
Owner <psimaron@gmail.com>
2023-03-15 17:47:51 +0200

    icon changed

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Item from '../Item/Item';
-import { ImCreditCard, ImSearch, ImUserCheck } from 'react-icons/im'
+import { ImSearch, ImUserCheck } from 'react-icons/im'
+import { RiPencilRulerLine } from 'react-icons/ri'
 
 class List extends React.Component {
 
     render () {
       return ( 
       <div>
-        <Item icon={<ImCreditCard />} title={"Brief"} text={"Complete brief writing or simple guidance on what to include, we've got you covored"}/>
+        <Item icon={<RiPencilRulerLine />} title={"Brief"} text={"Complete brief writing or simple guidance on what to include, we've got you covored"}/>
         <Item icon={<ImSearch />} title={"Search"} text={"In-depth agency search covering; criteria matching, door knocking and due-diligence vetting"}/>
         <Item icon={<ImUserCheck />} title={"Pitch"} text={"Comprehensive pitch management inculiding comms, diary management and pitch hosting"}/>
       </div>
